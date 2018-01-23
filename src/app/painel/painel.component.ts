729f3a3da9127fdc7a52e00b769de93ce007de56
jorge nunes <jorgezeza2010@gmail.com>
2018-01-23 12:14:31 -0200

    controle das tentativas

--- src/app/painel/painel.component.ts
+++ src/app/painel/painel.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output, OnDestroy } from '@angular/core';
+import { TentativasComponent } from './../tentativas/tentativas.component';
 import { Frase } from './../shared/frase.model';
 import { FRASES } from './frases-mock';
 
@@ -7,13 +8,17 @@ import { FRASES } from './frases-mock';
   templateUrl: './painel.component.html',
   styleUrls: ['./painel.component.css']
 })
-export class PainelComponent implements OnInit {
+export class PainelComponent implements OnInit, OnDestroy {
   public frases: Frase[] = FRASES
   public instrucao: string = "Traduza a Frase"
   public resposta: string = ""
 
   public rodada: number = 0
   public rodadaFrase: Frase
+  public tentativas: number = 3
+
+  @Output() public encerrarJogo: EventEmitter<string> = new EventEmitter()
+
 
   public progresso: number = 0
 
@@ -24,6 +29,10 @@ export class PainelComponent implements OnInit {
   ngOnInit() {
   }
 
+  ngOnDestroy() {
+
+  }
+
   public atualizaResposta(resposta: Event): void {
     this.resposta = (<HTMLInputElement>resposta.target).value
     //console.log(this.resposta)
@@ -31,27 +40,34 @@ export class PainelComponent implements OnInit {
 
 
   public verficarResposta(): void {
-
+    console.log(this.tentativas)
     if (this.rodadaFrase.frasePtBr == this.resposta) {
-      alert('A resposta está correta')
 
       // trocar a pergunta de rodada
       this.rodada++
 
       //progresso
-      this.progresso = this.progresso +  (100 / this.frases.length)
-      console.log(this.progresso)
+      this.progresso = this.progresso + (100 / this.frases.length)
+
+      if (this.rodada === 4) {
+        this.encerrarJogo.emit('vitoria')
+      }
 
       //Atualiza o objeto rodadaFrase
       this.atualizaRodada()
 
     } else {
-      alert('A resposta está errada')
+      //Diminuir a variavel tentativas
+      this.tentativas--
+
+      if (this.tentativas === -1) {
+        this.encerrarJogo.emit('derrota')
+      }
     }
   }
 
   //Define a frase da rodada com base em alguma lógica
-  public atualizaRodada():void {
+  public atualizaRodada(): void {
     this.rodadaFrase = this.frases[this.rodada]
     //limpar a resposta
     this.resposta = ""
--- src/app/tentativas/tentativas.component.ts
+++ src/app/tentativas/tentativas.component.ts
@@ -1,15 +1,15 @@
+import { Component, OnInit, Input,OnChanges } from '@angular/core';
 import { Coracao } from './../shared/coracao.model';
-import { Component, OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-tentativas',
   templateUrl: './tentativas.component.html',
   styleUrls: ['./tentativas.component.css']
 })
-export class TentativasComponent implements OnInit {
-  
-  public coracaoVazio: String = "/assets/coracao_vazio.png"
-  public coracaoCheio: String = "/assets/coracao_cheio.png"
+export class TentativasComponent implements OnInit, OnChanges {
+
+  @Input() public tentativas: number 
 
   public coracoes: Coracao[] = [
     new Coracao(true),
@@ -18,10 +18,18 @@ export class TentativasComponent implements OnInit {
   ]
 
   constructor() {
-    console.log(this.coracoes)
+    
+   }
+
+   ngOnChanges() {
+    if (this.tentativas !== this.coracoes.length) {
+      let indice = this.coracoes.length - this.tentativas
+      this.coracoes[indice -1].cheio = false
+    }
    }
 
   ngOnInit() {
+    
   }
 
 }
